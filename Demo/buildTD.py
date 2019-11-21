b396ee186b3a7936510454b26da72f67bd1085ae
DanieleRossi95 <rossid95@gmail.com>
2019-11-21 21:56:13 +0100

    refactoring del codice

--- Demo/buildTD.py
+++ Demo/buildTD.py
@@ -1,23 +1,61 @@
+# coding=utf-8
+
 import click
+import functools
+import json
 
-@click.group(invoke_without_command=True)
+#click.confirm('\nWould you use wizard?', default=True, abort=True)
 
-@click.pass_context
-def cli(ctx):
+# le funzioni non sono legate alle proprietà anche se ci accedono, 
+# per cui possono essere inserite in qualunque parte del codice
+
+# document contenente la TD
+x = {}
+
+# option in comune ai diversi comandi
+def common_options(f):
+    options = [
+        click.option('--nproperties', type=int, default=0, help='Number of properties of the Thing.'),
+        click.option('--thingname', type=str, help='Name of the Thing.'),
+    ]
+    return functools.reduce(lambda x, opt: opt(x), options, f)
+    
+
+# i tre parametri passati alle callback sono obbligatori anche se non vengono utilizzati
+# value è il valore assegnato alla proprietà
+def compute_properties(ctx, param, value):
+    click.echo('Number of properties: %s' % value)
+    # se non si ritorna il valore della proprietà, 
+    # alla variabile in cli corrispondente alla proprietà non viene assegnato nulla
+    # e di conseguenza il valore viene perso
+    return value
+
+@click.group()
+@common_options
+# cli è una callback del gruppo definito precedentemente
+def cli(**kwargs):
     """WoT module for build TDs and executable scripts for embedded systems"""
-    if ctx.invoked_subcommand is None:
-        click.echo('This module allow you to build custom Thing Descriptions and executable scripts for expose Thing on embedded systems')
-        click.echo('Use --help option to see documentation')
-        start()
-    else:
-        pass
+    click.echo('This module allow you to build custom Thing Descriptions and executable scripts for expose Thing on embedded systems')
+    click.echo('Use --help option to see documentation')
+    # appena termina il codice della cli viene eseguito il codice del comando start
+    
 
+# le funzioni che vengono eseguite dai comandi non possono essere chiamate al di fuori del comando stesso
+@cli.command()
+@common_options
+# se si passa il contesto ad una callback, allora come parametro le si deve passare ctx obbligatoriamente
+@click.pass_context
+def start(ctx, thingname, **kwargs):
+    """Start wizard"""
+    click.echo('\nWizard start...')
+    pippo = click.prompt('Inserisci pippo')
+    click.echo('pippo %s' % pippo)
 
-@click.command()
-
-@click.confirmation_option(prompt='\nWould you use wizard?')
-def start():
-    click.echo('Wizard start...')
 
 if __name__ == "__main__":
-   cli()  
+    # la funzione che viene richiamata nel main è la sola ad essere esguita dalla cli,
+    # per cui la cli vede solo le proprietà e gli argomenti da lei acceduti 
+    # che poi verranno visualizzati nella documentazione (help) 
+    # se si inserisce un'opzione prima del comando, allora viene gestita dalla cli, 
+    # se viene inserita dopo, viene gestita dal comando stesso
+    cli() 
