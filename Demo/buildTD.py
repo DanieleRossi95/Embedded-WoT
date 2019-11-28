4b6dd10b922c0e428c5ac95725402dc66a89d7b7
DanieleRossi95 <rossid95@gmail.com>
2019-11-28 14:00:28 +0100

    Aggiunti tipi custom tramite Click Classes per gestire l'input dell'utente

--- Demo/buildTD.py
+++ Demo/buildTD.py
@@ -3,14 +3,63 @@
 import click
 import functools
 import json
+import jsonschema as js
+import ast
 
-#click.confirm('\nWould you use wizard?', default=True, abort=True)
 
 # le funzioni non sono legate alle proprietà anche se ci accedono, 
 # per cui possono essere inserite in qualunque parte del codice
 
+class StartWithoutNumberStringParamType(click.ParamType):
+    name = 'string'
+
+    def convert(self, value, param, ctx):
+        try:
+            if(value[0].isdigit() == False):
+                return str(value)
+            else:
+                raise ValueError
+        except TypeError:
+            self.fail('Expected string, got {a} of type {b}'.format(a=value, b=type(value).__name__), param, ctx)
+        except ValueError:
+            self.fail('Thing title MUST start with a character, not with a number', param, ctx)
+
+
+class NotZeroIntParamType(click.ParamType):
+    name = 'integer'
+
+    def convert(self, value, param, ctx):
+        try:
+            if(value != "0"):
+                return int(value)
+            else:
+                raise ValueError
+        except TypeError:
+            self.fail('Expected string for int() conversion, got {a} of type {b}'.format(a=value, b=type(value).__name__), param, ctx)
+        except ValueError:
+            self.fail('0 is not an allowed number', param, ctx)
+
+
+class ObjectStringParamType(click.ParamType):
+    name = 'dict'
+
+    def convert(self, value, param, ctx):
+        try:
+            return ast.literal_eval(value)
+        except Exception:
+            self.fail('Element format is incorrect\n', param, ctx)
+
+
+SWN_STRING = StartWithoutNumberStringParamType()
+NZ_INT = NotZeroIntParamType()
+OBJ_STRING = ObjectStringParamType()
+
+# JSON SCHEMA per la TD
+schema = json.load(open('prova_schema.json'))
+
 # document contenente la TD
 x = {}
+
 
 # option in comune ai diversi comandi
 def common_options(f):
@@ -30,13 +79,19 @@ def compute_properties(ctx, param, value):
     # e di conseguenza il valore viene perso
     return value
 
-@click.group()
+@click.group(invoke_without_command=True)
 @common_options
+@click.pass_context
 # cli è una callback del gruppo definito precedentemente
-def cli(**kwargs):
+def cli(ctx, **kwargs):
     """WoT module for build TDs and executable scripts for embedded systems"""
     click.echo('This module allow you to build custom Thing Descriptions and executable scripts for expose Thing on embedded systems')
     click.echo('Use --help option to see documentation')
+    if(ctx.invoked_subcommand is None):
+        click.confirm('\nUse the wizard?', default=True, abort=True)
+        ctx.invoke(start)
+    else:
+        pass        
     # appena termina il codice della cli viene eseguito il codice del comando start
     
 
@@ -47,9 +102,35 @@ def cli(**kwargs):
 @click.pass_context
 def start(ctx, thingname, **kwargs):
     """Start wizard"""
+    ctx.ensure_object(dict)
     click.echo('\nWizard start...')
-    pippo = click.prompt('Inserisci pippo')
-    click.echo('pippo %s' % pippo)
+    # THING TITLE
+    thingTitle = click.prompt('\nThing Title', type=SWN_STRING)
+    ctx.obj['title'] = thingTitle     
+    # THING CONTEXT
+    uri = 'https://www.w3.org/2019/wot/td/v1'
+    if(click.confirm('\nUse the default context?', default=True)):
+        ctx.obj['@context'] = uri
+    else:
+        contextElement = click.prompt('\nContext elements number', type=NZ_INT)
+        click.echo('Warning: String context element MUST be enclosed by single or double quotes')
+        click.echo('Mandatory element: %s \n' % uri)
+        # se all'interno di un dizionario (ctx.obj) si vuole definire un array a cui aggiungere un
+        # elemento alla volta, si possono utilizzare i metodi setdeafult per creare l'array e 
+        # append per aggiungere gli elementi
+        ctx.obj.setdefault('@context', [])
+        for i in range(1, contextElement+1):
+            inp = click.prompt('Insert element %d' % i, type=OBJ_STRING)
+            ctx.obj['@context'].append(inp)
+    # THING TYPE
+    if(click.confirm('\nInsert Thing type?', default=True)):
+        inp = click.prompt('Scelta', type=click.Choice([1,2]), show_choices=True)
+    try:
+        js.validate(ctx.obj, schema)
+    except Exception as e:
+        click.echo(str(e))
+    click.echo(ctx.obj)
+    
 
 
 if __name__ == "__main__":
